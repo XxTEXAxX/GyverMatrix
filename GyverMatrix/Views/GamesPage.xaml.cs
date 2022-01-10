6c5bec1ee569bc58a0a9ed4435eec116ec074aa5
XxTEXAxX <64602518+XxTEXAxX@users.noreply.github.com>
2022-01-10 20:28:58 +0300

    GamesPage

--- GyverMatrix/Views/GamesPage.xaml.cs
+++ GyverMatrix/Views/GamesPage.xaml.cs
@@ -12,8 +12,24 @@ namespace GyverMatrix.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GamesPage : ContentPage
     {
-        public GamesPage()
+
+        Dictionary<string, int> nameToGame = new Dictionary<string, int>
         {
+            { "Змейка", 0 },
+            { "Тетрис", 1 }, 
+            { "Динозаврик", 2 }, 
+            { "Лабиринт", 3 }, 
+            { "Арканоид", 4 }, 
+        };
+        
+    public GamesPage()
+        {
+
+            //foreach (string gameName in nameToGame.Keys)
+            //{
+            //    Games.Items.Add(gameName);
+            //}
+
             InitializeComponent();
         }
     }
