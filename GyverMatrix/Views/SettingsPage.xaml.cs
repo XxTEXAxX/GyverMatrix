6a6e0b2078c4b4fb93b7655512933b46e9bb96d8
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-10 21:28:04 +0300

    Fix parse

--- GyverMatrix/Views/SettingsPage.xaml.cs
+++ GyverMatrix/Views/SettingsPage.xaml.cs
@@ -1,74 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace GyverMatrix.Views
-{
+namespace GyverMatrix.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SettingsPage : ContentPage
-    {
-        public SettingsPage()
-        {
+    public partial class SettingsPage {
+        public SettingsPage() =>
             InitializeComponent();
 
-            
+        private void DemoSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
 
         }
 
-        private void BrightnessChanged(object sender, ValueChangedEventArgs e)
-        {
-            int value = (int) e.NewValue;
-            BrightnessText.Text = value.ToString();
-        }
-
-        private void DemoSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
+        private void Undo_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void Undo_Clicked(object sender, EventArgs e)
-        {
+        private void Next_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void Next_Clicked(object sender, EventArgs e)
-        {
+        private void AutoCheck1_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void AutoCheck1_Clicked(object sender, EventArgs e)
-        {
+        private void AutoCheck2_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void AutoCheck2_Clicked(object sender, EventArgs e)
-        {
+        private void AutoCheck3_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void AutoCheck3_Clicked(object sender, EventArgs e)
-        {
+        private void NetSet1_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void NetSet1_Clicked(object sender, EventArgs e)
-        {
+        private void NetSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
 
         }
 
-        private void NetSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
+        private void NetSet2_Clicked(object sender, EventArgs e) {
 
         }
 
-        private void NetSet2_Clicked(object sender, EventArgs e)
-        {
-
-        }
+        private void BrightnessSlider_OnValueChanged(object sender, ValueChangedEventArgs e) =>
+            BrightnessText.Text = ((int)((Slider)sender).Value).ToString();
     }
 }
