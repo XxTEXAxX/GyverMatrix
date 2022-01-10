ae837c01aeb054cb4e88f361c3bb52e91a61f504
XxTEXAxX <64602518+XxTEXAxX@users.noreply.github.com>
2022-01-10 22:56:34 +0300

    EffectsPage UPD, GamePage Font UPD

--- GyverMatrix/Views/EffectsPage.xaml.cs
+++ GyverMatrix/Views/EffectsPage.xaml.cs
@@ -16,5 +16,25 @@ namespace GyverMatrix.Views
         {
             InitializeComponent();
         }
+
+        private void EffectSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void HourSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            BrightnessText.Text = ((int)((Slider)sender).Value).ToString();
+        }
+
+        private void SpeedSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SpeedText.Text = ((int)((Slider)sender).Value).ToString();
+        }
     }
 }
