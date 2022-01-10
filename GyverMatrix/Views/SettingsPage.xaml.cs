077c8a58b9dab4a1fed0a1b684b37a894074e87a
XxTEXAxX <64602518+XxTEXAxX@users.noreply.github.com>
2022-01-10 17:52:44 +0300

    SettingsPage
    Страница настроек xaml написана

--- GyverMatrix/Views/SettingsPage.xaml.cs
+++ GyverMatrix/Views/SettingsPage.xaml.cs
@@ -15,6 +15,60 @@ namespace GyverMatrix.Views
         public SettingsPage()
         {
             InitializeComponent();
+
+            
+
+        }
+
+        private void BrightnessChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int) e.NewValue;
+            BrightnessText.Text = value.ToString();
+        }
+
+        private void DemoSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void Undo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutoCheck1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutoCheck2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutoCheck3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NetSet1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NetSwitch_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void NetSet2_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
