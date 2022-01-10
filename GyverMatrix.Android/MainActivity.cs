cbefa9b6b405f20a4ed8f252505807e6ca799b69
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-10 21:02:45 +0300

    Fix font

--- GyverMatrix.Android/MainActivity.cs
+++ GyverMatrix.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace GyverMatrix.Droid
 {
-    [Activity(Label = "GyverMatrix", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "GyverMatrix", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
