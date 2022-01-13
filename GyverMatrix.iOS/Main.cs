c69d794ac2824c63cba990aedf3a58773ad0c3b9
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-13 11:29:17 +0300

    Add splash screen for ios, fix parse in settings

--- GyverMatrix.iOS/Main.cs
+++ GyverMatrix.iOS/Main.cs
@@ -2,8 +2,6 @@
 
 namespace GyverMatrix.iOS {
     public class Application {
-        private static void Main(string[] args) {
-            UIApplication.Main(args, null, "AppDelegate");
-        }
+        private static void Main(string[] args) => UIApplication.Main(args, null, "AppDelegate");
     }
 }
