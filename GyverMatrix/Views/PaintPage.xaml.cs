43703698b80fb92b956acb973fe707977a0e8d38
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-13 15:11:21 +0300

    To Android 11

--- GyverMatrix/Views/PaintPage.xaml.cs
+++ GyverMatrix/Views/PaintPage.xaml.cs
@@ -1,9 +1,18 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace GyverMatrix.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PaintPage {
         public PaintPage() =>
             InitializeComponent();
+
+        public int Touch { get; set; }
+
+        private void PanGestureRecognizer_OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            Console.WriteLine($"Pan {Touch}");
+        }
     }
 }
