1cdd8614a181fcef95ce1e55bc7be81381f098a6
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-16 12:55:14 +0300

    Add chaange theme

--- GyverMatrix/App.xaml.cs
+++ GyverMatrix/App.xaml.cs
@@ -1,8 +1,18 @@
-﻿namespace GyverMatrix {
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GyverMatrix {
     public partial class App {
         public App() {
             InitializeComponent();
             MainPage = new AppShell();
+        }
+
+        private async void App_OnPageAppearing(object sender, Page e) {
+            if (await SecureStorage.GetAsync("Theme") != null)
+                return;
+            await SecureStorage.SetAsync("Theme", "Dark");
+            UserAppTheme = OSAppTheme.Dark;
         }
     }
 }
