bf11fe4f44035c89893203fee2abb40175c3e24f
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-01-19 22:06:51 +0300

    Add colorPicker

--- GyverMatrix/Views/PaintPage.xaml.cs
+++ GyverMatrix/Views/PaintPage.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Threading;
+﻿using ColorPicker;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace GyverMatrix.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PaintPage {
+    public partial class PaintPage : INotifyPropertyChanged {
         //Dictionary<long, SKPath> inProgressPaths = new Dictionary<long, SKPath>();
         //List<SKPath> completedPaths = new List<SKPath>();
         //SKPaint paint = new SKPaint {
@@ -20,28 +22,28 @@ namespace GyverMatrix.Views {
         }
         private Frame[,] _frames;
         private double _size;
-        private Color _currentColor = Color.DarkOrange;
+        public Color CurrentColor { get; set; } = Color.DarkOrange;
 
         private PaintModes _currentMode = PaintModes.Brush;
         //int h = int.Parse(await SecureStorage.GetAsync("H"));
         //int w = int.Parse(await SecureStorage.GetAsync("W"));
-        int h = 21, w = 16;
+        int _h = 21, _w = 16;
         public PaintPage() =>
             InitializeComponent();
 
         private void PaintPage_OnAppearing(object sender, EventArgs e) {
-            _size = (Application.Current.MainPage.Width / w / 1.1);
-            for (int i = 0; i < w; i++) {
+            _size = (Application.Current.MainPage.Width / _w / 1.1);
+            for (int i = 0; i < _w; i++) {
                 CustomGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_size) });
             }
 
-            for (int i = 0; i < h; i++) {
+            for (int i = 0; i < _h; i++) {
                 CustomGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(_size) });
             }
 
-            _frames = new Frame[h, w];
-            for (int r = 0; r < h; r++) {
-                for (int c = 0; c < w; c++) {
+            _frames = new Frame[_h, _w];
+            for (int r = 0; r < _h; r++) {
+                for (int c = 0; c < _w; c++) {
                     Frame btn = new Frame {
                         BorderColor = Color.DarkOrange,
                         BackgroundColor = Color.Transparent,
@@ -59,10 +61,10 @@ namespace GyverMatrix.Views {
             try {
                 if (!(args.Location.X > 0) || !(args.Location.Y > 0) || !(args.Location.Y < CustomGrid.Height) || !(args.Location.X < CustomGrid.Width))
                     return;
-                var column = (int)(args.Location.X / _size);
-                var row = (int)(args.Location.Y / _size);
+                var column = (int)Math.Ceiling(args.Location.X / _size) - 1;
+                var row = (int)Math.Ceiling(args.Location.Y / _size) - 1;
                 _frames[row, column].BackgroundColor = _currentMode switch {
-                    PaintModes.Brush => _currentColor,
+                    PaintModes.Brush => CurrentColor,
                     _ => Color.Transparent
                 };
             } catch {
@@ -71,9 +73,9 @@ namespace GyverMatrix.Views {
         }
 
         private void Bucket_Clicked(object sender, EventArgs e) {
-            for (int r = 0; r < h; r++) {
-                for (int c = 0; c < w; c++) {
-                    _frames[r, c].BackgroundColor = _currentColor;
+            for (int r = 0; r < _h; r++) {
+                for (int c = 0; c < _w; c++) {
+                    _frames[r, c].BackgroundColor = CurrentColor;
                 }
             }
         }
@@ -90,12 +92,33 @@ namespace GyverMatrix.Views {
             _currentMode = PaintModes.Brush;
         }
 
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e) {
+            PickerBlock.IsVisible = true;
+            GridBlock.IsVisible = false;
+        }
+
+        private void CloseColorPicker_Clicked(object sender, EventArgs e) {
+            PickerBlock.IsVisible = false;
+            GridBlock.IsVisible = true;
+        }
+
         private void Clear_Clicked(object sender, EventArgs e) {
-            for (int r = 0; r < h; r++) {
-                for (int c = 0; c < w; c++) {
+            for (int r = 0; r < _h; r++) {
+                for (int c = 0; c < _w; c++) {
                     _frames[r, c].BackgroundColor = Color.Transparent;
                 }
             }
         }
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void ColorTriangle_SelectedColorChanged(object sender, ColorPicker.BaseClasses.ColorPickerEventArgs.ColorChangedEventArgs e) {
+            ColorTriangle colorPicker = (ColorTriangle)sender;
+            CurrentColor = colorPicker.SelectedColor;
+            NotifyPropertyChanged(nameof(CurrentColor));
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
