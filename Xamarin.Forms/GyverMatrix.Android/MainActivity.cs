494166bb8c8801785dc8902407100cff1ad77a8d
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2022-12-03 12:15:28 +0300

    New realization SplashScreen

--- Xamarin.Forms/GyverMatrix.Android/MainActivity.cs
+++ Xamarin.Forms/GyverMatrix.Android/MainActivity.cs
@@ -3,18 +3,23 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
-namespace GyverMatrix.Droid {
-    [Activity(Label = "GyverMatrix", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
-        protected override void OnCreate(Bundle savedInstanceState) {
+namespace GyverMatrix.Droid
+{
+    [Activity(Label = "GyverMatrix", Icon = "@mipmap/icon", Theme = "@style/Splash", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
