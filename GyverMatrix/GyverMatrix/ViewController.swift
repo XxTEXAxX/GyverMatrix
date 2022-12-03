790190f427e7e19a2fce1be749f759714f5c1016
Andrey Rybalkin <andreysavie@ya.ru>
2022-12-03 16:37:14 +0300

    Initial commit

---
+++ GyverMatrix/GyverMatrix/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GyverMatrix
+//
+//  Created by Андрей Рыбалкин on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
